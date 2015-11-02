38ad6b0f827b01bca5eff7df740a8e4e29f6c223
Jacob Blakely <jacob@codekirei.com>
2015-11-02 14:13:07 -0800

    write tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = function(line, indent, limit, accum) {
+module.exports = function hangingIndent(line, indent, limit, accum) {
   // params and defaults
   indent = indent ? indent : 4
   limit = limit ? limit : 80
@@ -9,7 +9,7 @@ module.exports = function(line, indent, limit, accum) {
   // if line is too long
   if (line.length > limit) {
     // get longest possible substring from line
-    const reverseSlice = line.slice(0, limit - 1).split('').reverse('')
+    const reverseSlice = line.slice(0, limit).split('').reverse('')
     const firstSpace = reverseSlice.indexOf(' ')
     const longest = reverseSlice.slice(firstSpace + 1).reverse().join('')
     const rem = ' '.repeat(indent) + line.slice(longest.length + 1)
@@ -23,6 +23,3 @@ module.exports = function(line, indent, limit, accum) {
   // else append line to accum
   return accum += line
 }
-
-// console.log(hangingIndent(testStr))
-// console.log(hangingIndent(testStr).split('\n').map(line => line.length))
--- test/index.js
+++ test/index.js
@@ -3,20 +3,78 @@
 //----------------------------------------------------------
 // Modules
 //----------------------------------------------------------
-const chai = require('chai').assert
+const assert = require('chai').assert
 const faker = require('faker')
 const hangingIndent = require('../')
 
 //----------------------------------------------------------
 // Tests
 //----------------------------------------------------------
-const sampleText = faker.fake('{{lorem.paragraphs}}').split('\n').join(' ')
+// create sample texts
+const texts = []
+for (let i = 0; i < 25; i++) {
+  texts.push(
+    faker.fake('{{lorem.paragraphs}}')
+      .replace(/\n/g, ' ')
+      .replace(/\s{2}\r/g, ' ')
+  )
+}
+
 describe('params', () => {
-  it('use default when unspecified')
-  it('override default when specified')
+  describe('indent', () => {
+    function testCase(indent) {
+      return texts
+        .map(text => {
+          return indent
+            ? hangingIndent(text, indent)
+            : hangingIndent(text)
+        })
+        .map(text => text.split('\n'))
+        .map(lines => {
+          for (let i = 1; i < lines.length; i++) {
+            indent
+              ? assert.isTrue(lines[i].startsWith(' '.repeat(indent)))
+              : assert.isTrue(lines[i].startsWith(' '.repeat(4)))
+          }
+        })
+    }
+
+    it('use default', () => testCase())
+    it('override with larger param', () => testCase(6))
+    it('override with smaller param', () => testCase(2))
+  })
+
+  describe('limit', () => {
+    function testCase(limit) {
+      return texts
+        .map(text => {
+          return limit
+            ? hangingIndent(text, 4, limit)
+            : hangingIndent(text)
+        })
+        .map(text => text.split('\n'))
+        .map(lines => {
+          lines.map(line => {
+            limit
+              ? assert.isTrue(line.length <= limit)
+              : assert.isTrue(line.length <= 80)
+          })
+        })
+    }
+    it('use default', () => testCase())
+    it('override with larger param', () => testCase(100))
+    it('override with smaller param', () => testCase(50))
+  })
 })
+
 describe('lines', () => {
-  it('none over limit')
-  it('indent all except first')
-  it('all but last not same length')
+  it('all line lengths except last not equal to limit', () => {
+    texts.map(text => hangingIndent(text))
+      .map(text => text.split('\n'))
+      .map(lines => lines.map(line => line.length))
+      .map(lengths => lengths.filter((el, i) => i !== lengths.length - 1))
+      .map(lengths => {
+        assert.isFalse(lengths.every(length => length === 80))
+      })
+  })
 })
